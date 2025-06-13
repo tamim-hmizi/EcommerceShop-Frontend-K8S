6feef9f65d7e96baa889c52f56ce983efcde9e4f
Tamim Hmizi <hmizitamim@hotmail.com>
2025-06-13 06:00:25 +0100

    Update categoryService.js

--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -3,7 +3,7 @@ import api from "./api";
 export const getCategories = async () => {
   try {
     console.log("Fetching categories");
-    const res = await api.get("/category");
+    const res = await api.get("/categories");
 
     console.log("Categories API response:", res);
 
@@ -26,26 +26,26 @@ export const getCategories = async () => {
 };
 
 export const getCategory = async (_id) => {
-  const res = await api.get(`/category/${_id}`);
+  const res = await api.get(`/categories/${_id}`);
   return res.data;
 };
 
 export const createCategory = async (data, token) => {
-  const res = await api.post("/category", data, {
+  const res = await api.post("/categories", data, {
     headers: { Authorization: `Bearer ${token}` },
   });
   return res.data;
 };
 
 export const updateCategory = async (_id, data, token) => {
-  const res = await api.put(`/category/${_id}`, data, {
+  const res = await api.put(`/categories/${_id}`, data, {
     headers: { Authorization: `Bearer ${token}` },
   });
   return res.data;
 };
 
 export const deleteCategory = async (_id, token) => {
-  const res = await api.delete(`/category/${_id}`, {
+  const res = await api.delete(`/categories/${_id}`, {
     headers: { Authorization: `Bearer ${token}` },
   });
   return res.data;
